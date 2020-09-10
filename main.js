67ce5759230fb9eb0c79289cf08e7241326c859d
Huseyin Yilmaz <ylmzhsyn98@gmail.com>
2020-09-10 19:52:47 +0300

    port change

--- main.js
+++ main.js
@@ -1,27 +1,33 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const fs = require('fs');
-const https = require('https');
-const http = require('http');
+const fs = require("fs");
+const https = require("https");
+const http = require("http");
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "build")));
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("/", (req, res) => {
+    console.log(req.headers["host"]);
+    res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-https.createServer({
-    key: fs.readFileSync('private.key'),
-    cert: fs.readFileSync('certificate.crt'),
-    ca: fs.readFileSync('ca_bundle.crt')
-}, app)
-    .listen(443, function () {
-        console.log('app working on port 443')
+https
+    .createServer(
+        {
+            key: fs.readFileSync("private.key"),
+            cert: fs.readFileSync("certificate.crt"),
+            ca: fs.readFileSync("ca_bundle.crt"),
+        },
+        app
+    )
+    .listen(443, () => console.log("app working on port 443"));
+
+http
+    .createServer((req, res) => {
+        res.writeHead(301, {
+            Location: "https://" + req.headers["host"] + req.url,
+        });
+        res.end();
     })
-
-
-http.createServer(function (req, res) {
-    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
-    res.end();
-}).listen(80,()=>console.log("also 80"));
+    .listen(80, () => console.log("also 80"));
